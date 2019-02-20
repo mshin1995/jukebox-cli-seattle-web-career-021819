aa0dd6cc07318da47665933f6565b1a32b9c68d4
Matthew Shin <mshin1995@gmail.com>
2019-02-20 04:37:02 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -11,7 +11,11 @@ songs = [
 ]
 
 def help 
-  
+  puts "I accept the following commands:
+    - help : displays this help message
+    - list : displays a list of songs you can play
+    - play: lets you choose a song to play
+    - exit : exits this program"
 end
 
 def play
