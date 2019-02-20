77e5cf5c5d61f3900f42f78cc4ab26b535fcfc92
Matthew Shin <mshin1995@gmail.com>
2019-02-20 04:55:27 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -40,7 +40,7 @@ def exit_jukebox
   puts "Goodbye"
 end
 
-def run
+def run(songs)
   help
   puts "Please enter a command:"
   user_response = gets.chomp
@@ -54,6 +54,6 @@ def run
   when "exit"
     exit_jukebox
   else
-    run
+    run(songs)
   end
 end
